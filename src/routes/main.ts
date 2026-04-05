45034bdb4e6096f53a6f336ac8fcc607512b2a77
Toddy <jaelsonkaciel4@gmail.com>
2026-04-04 23:15:46 -0300

    deleted main

--- src/routes/main.ts
+++
@@ -1,9 +0,0 @@
-import { RouteOptions } from 'fastify';
-
-export default {
-    method: 'GET', // use array for multiple methods
-    url: '/',
-    handler: (request, reply) => {
-        return reply.send('Hello World!');
-    },
-} as RouteOptions;
